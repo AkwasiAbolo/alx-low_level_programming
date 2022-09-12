09a71da6c3253bffeb73f6dbd10f9f3ac7f6651e
ahmed <mrboateng49@gmail.com>
2022-09-12 14:59:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ * void empty
+ * return (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= z; c++)
+
+	{
+		putcher(c);
+
+	}
+
+	putcher('\n');
+
+	return (0);
+
+}
