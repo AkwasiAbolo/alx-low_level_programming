fb05a4aa9e7fdd296c4a784b206658df94f9eb7a
ahmed <mrboateng49@gmail.com>
2022-09-20 23:01:54 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts_half - printing half of string
+ * @str : array variable
+ * return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
